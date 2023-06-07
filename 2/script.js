decce3d504bca125f5622b8e38ccbd0a357ce744
Paulina Casé <134965538+casepaulina@users.noreply.github.com>
2023-06-07 16:14:11 -0300

    Add files via upload

---
+++ 2/script.js
@@ -0,0 +1,12 @@
+const Menu = document.querySelector('.menu');
+
+let lineTop = document.querySelector('.line1');
+let line = document.querySelector('.line2');
+let lineButtom = document.querySelector('.line3');
+
+
+Menu.addEventListener('click', ()=> {
+   lineTop.classList.toggle("top");
+   line.classList.toggle("centro");
+   lineButtom.classList.toggle("buttom");
+})
